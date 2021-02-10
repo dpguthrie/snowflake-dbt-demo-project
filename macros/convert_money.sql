342d49126d274f53db0fc10707203471b01bf111
Matt Winkler <matt.winkler@fishtownanalytics.com>
2021-02-10 19:27:51 +0000

    bring up to date

--- macros/convert_money.sql
+++ macros/convert_money.sql
@@ -1,3 +1,10 @@
 {% macro money(col) -%}
 ::decimal(16,4)
 {%- endmacro %}
+
+{% macro matts_awesome_macro(var1, var2) -%}
+
+{{ var1 }}
+
+{%- endmacro %}
+
--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -4,6 +4,7 @@
         tags=['finance']
     )
 }}
+
 with orders as (
     
     select * from {{ ref('stg_tpch_orders') }} 
