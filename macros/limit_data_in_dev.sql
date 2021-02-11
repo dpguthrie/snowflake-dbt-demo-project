321e931e5efe5316a492f04228761bd949f76a59
Matt Winkler <matt.winkler@fishtownanalytics.com>
2021-02-11 20:41:17 +0000

    added some spaces

--- macros/limit_data_in_dev.sql
+++ macros/limit_data_in_dev.sql
@@ -1,6 +1,8 @@
 {% macro limit_data_in_dev(filter_column_name, lookback_days=7) %}
 
 {% if target.name == 'dev' %}
+
+
 where {{ filter_column_name }} >= dateadd('day', -{{ lookback_days }}, current_timestamp)
 {% endif %}
 
