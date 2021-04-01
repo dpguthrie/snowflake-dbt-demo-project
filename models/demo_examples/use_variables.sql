b4c63f9373b47533bdea0c9f0104148d6989b863
Matt Winkler <matt.winkler@fishtownanalytics.com>
2021-04-01 22:04:37 +0000

    made a change to use_variables

--- models/demo_examples/use_variables.sql
+++ models/demo_examples/use_variables.sql
@@ -5,3 +5,5 @@
 -- start_date is defined in the dbt_project.yml 
 -- to illustrate overriding variables from the command line, run dbt run -m use_variables --vars '{"start_date": "1996-01-01"}'
 select * from {{ ref('fct_order_items') }} where order_date >= '{{ var("start_date") }}'
+
+
