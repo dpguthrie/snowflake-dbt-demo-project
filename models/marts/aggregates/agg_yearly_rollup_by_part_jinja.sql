b6bbfc7b094ef794321bc53ab7c9409fae8ad889
Sean McIntyre <sean.mcintyre@fishtownanalytics.com>
2021-04-26 16:26:40 +0000

    Adjust comments and indentation of pivot examples

--- models/marts/aggregates/agg_yearly_rollup_by_part_jinja.sql
+++ models/marts/aggregates/agg_yearly_rollup_by_part_jinja.sql
@@ -1,38 +1,35 @@
-/*  Manually driven approach to mapping part names for iteration - this is automated by the call statement below
-
-  {% set part_names = ['goldenrod lavender spring chocolate lace',
-                     'blush thistle blue yellow saddle',
-                     'spring green yellow purple cornsilk',
-                     'cornflower chocolate smoke green pink',
-                     'forest brown coral puff cream'] %} */
+/* Create a pivot table with dynamic columns based on the parts that are in the system */
 
 {%- call statement('result', fetch_result=True) -%}
- -- this pulls the unique part names from the dim_part table
- select name from {{ ref('dim_parts') }} group by 1 
+
+    {# this pulls the unique part names from the dim_part table #}
+    select name from {{ ref('dim_parts') }} group by 1 
 
 {%- endcall %}
 
-{% set part_names = load_result('result').table.columns[0].values()[0:10] %}               
+{# Only fetch the first 10 since there are so many! #}
+{% set part_names = load_result('result').table.columns[0].values()[0:10] %}
 
-with fct_order_items as ( select * from {{ ref('fct_order_items') }} ),
+with fct_order_items as (
+    select * from {{ ref('fct_order_items') }}
+),
 
-dim_parts as ( select * from {{ ref('dim_parts') }} )
+dim_parts as (
+    select * from {{ ref('dim_parts') }}
+)
 
-select date_part('year', order_date) as order_year,
-       
-       {# loop over part_names array from above #}
-       {%- for part_name in part_names -%}
+select
+    date_part('year', order_date) as order_year,
 
-         {# map the part name for individual sum by date #}
-         sum(case when dp.name = '{{part_name}}' then gross_item_sales_amount end) as "{{part_name}}_amount"
-         {%- if not loop.last -%}
-         {# attach commas for all but the last line #}
-         ,
-         {%- endif -%}
-        
-       {%- endfor %}
+    {# Loop over part_names array from above, and sum based on whether the record matches the part name #}
+    {%- for part_name in part_names -%}
+        sum(case when dim_parts.name = '{{part_name}}' then gross_item_sales_amount end) as "{{part_name}}_amount"
+        {%- if not loop.last -%},{% endif %}
+    {% endfor %}
 
-from   fct_order_items foi
-inner join dim_parts dp 
-  on foi.part_key = dp.part_key 
+from
+    fct_order_items
+    inner join dim_parts
+        on fct_order_items.part_key = dim_parts.part_key 
+
 group by 1
