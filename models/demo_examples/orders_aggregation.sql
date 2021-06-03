2769f4532941eb47e798d729341d960f6995f9d0
Matt Winkler <matt.winkler@fishtownanalytics.com>
2021-06-03 03:38:12 +0000

    update unit tests

--- models/demo_examples/orders_aggregation.sql
+++ models/demo_examples/orders_aggregation.sql
@@ -2,7 +2,7 @@
 with order_items as (select * from {{ ref('fct_order_items') }} )
 
 -- good query
--- select part_key, sum(quantity) as quantity from order_items group by 1
+select part_key, sum(quantity) as quantity from order_items group by 1
 
 -- bad query
-select part_key, count(quantity) as quantity from order_items group by 1
+-- select part_key, count(quantity) as quantity from order_items group by 1
