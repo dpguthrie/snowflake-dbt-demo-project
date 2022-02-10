0f85d48de09210bb3300aec282f618d6f1d3fc48
Doug Guthrie <doug.guthrie@fishtownanalytics.com>
2022-02-10 16:42:09 +0000

    Add amount field to my_first_model

--- models/staging/tpch/my_first_model.sql
+++ models/staging/tpch/my_first_model.sql
@@ -2,11 +2,11 @@ with
 
 final as (
 
-    select 1 as id
+    select 1 as id, 1000 as amount
     union all
-    select 2 as id
+    select 2 as id, 1250 as amount
     union all
-    select null as id
+    select null as id, -1000 as amount
 
 )
 
