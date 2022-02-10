98a8942e4d81a0a52eabaf986b28e8d05dd4fa0c
Doug Guthrie <doug.guthrie@fishtownanalytics.com>
2022-02-10 18:44:43 +0000

    Fix snapshot

--- snapshots/customer_snapshot.sql
+++ snapshots/customer_snapshot.sql
@@ -1,16 +1,16 @@
 {% snapshot customer_snapshot %}
-​
+
 {{
-    config(
-      target_database='analytics',
-      target_schema='snapshots', 
-      unique_key='c_custkey',
-      strategy='timestamp',
-      updated_at='_updated_at',
-      transient=false
-    )
+  config(
+    target_database='analytics',
+    target_schema='snapshots',
+    unique_key='c_custkey',
+    strategy='timestamp',
+    updated_at='_updated_at',
+    transient=False
+  )
 }}
-​
+
 select * from {{ source('tpch_snapshot', 'customer_snapshot_src') }}
-​
-{% endsnapshot %} 
+
+{% endsnapshot %}
