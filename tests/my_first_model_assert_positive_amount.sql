fac0084ed6fdc018d45a5a74932afd876b088476
Doug Guthrie <doug.guthrie@fishtownanalytics.com>
2022-02-10 18:32:34 +0000

    Update demo_examples yml to exclude custom test

--- tests/my_first_model_assert_positive_amount.sql
+++ tests/my_first_model_assert_positive_amount.sql
@@ -1,3 +1,16 @@
+{{
+    config(
+        enabled=true,
+        severity='error'
+    )
+}}
+
+/*
+The assumption is that my_first_model should contain zero rows with
+an amount < 0
+
+So, our query should look to return any rows that disprove our assumption
+*/
 with model_data as ( select * from {{ ref('my_first_model') }} )
 
 select *
