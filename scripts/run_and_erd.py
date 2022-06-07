f94e7efd2bc69b9fec51e149cdf61b5e4b3f76fd
Doug Guthrie <douglas.p.guthrie@gmail.com>
2022-06-07 11:07:14 -0600

    Revert back to using snowflake-sqlalchemy

--- scripts/run_and_erd.py
+++ scripts/run_and_erd.py
@@ -4,9 +4,9 @@ import os
 # third party
 from dbtc import dbtCloudClient as dbtc
 from eralchemy import render_er
+from snowflake.sqlalchemy import URL
 
 
-# Put all of the schemas here that you'd like to generate an ERD for
 SCHEMAS = ['TPCH']
 
 
@@ -15,10 +15,10 @@ if __name__ == '__main__':
     account_id = os.getenv('DBT_CLOUD_ACCOUNT_ID')
     job_id = os.getenv('DBT_CLOUD_JOB_ID')
     
-    # Initialize dbtCloudClient with appropriate token
+    # Initialize dbtCloudClient with appropriate tokens
     client = dbtc(
-        api_key=os.getenv('DBT_CLOUD_API_KEY'),
         service_token=os.getenv('DBT_CLOUD_SERVICE_TOKEN'),
+        api_key=os.getenv('DBT_CLOUD_API_KEY')
     )
     
     # Trigger Job and Poll until successful
@@ -28,14 +28,16 @@ if __name__ == '__main__':
     
     print(f'View run here:  https://cloud.getdbt.com/#/accounts/{account_id}/projects/88168/runs/{run_id}/')
     
-    # Retrieve the URI stored as a secret
-    db_uri = os.getenv('DB_URI', None)
-    
-    # Ensure that it's set
-    if db_uri is not None:
-
-        for schema in SCHEMAS:
-            uri = db_uri.format(**{'schema': schema})
-            render_er(uri, f'assets/{schema.lower()}_erd.png')
+    for schema in SCHEMAS:
+        url = URL(
+            account=os.getenv('SF_ACCOUNT'),
+            user=os.getenv('SF_USER'),
+            password=os.getenv('SF_PASSWORD'),
+            database='DOUG_DEMO_V2',
+            schema=schema,
+            warehouse='TRANSFORMING',
+            role='TRANSFORMER',
+        )
+        render_er(url.__str__(), f'assets/{schema.lower()}_erd.png')
         
-        print(f'View docs here:  https://cloud.getdbt.com/#/accounts/{account_id}/jobs/{job_id}/#!/overview')
+    print(f'View docs here:  https://cloud.getdbt.com/#/accounts/{account_id}/jobs/{job_id}/#!/overview')
