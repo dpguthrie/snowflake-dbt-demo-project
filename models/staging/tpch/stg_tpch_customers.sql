88667f38e6a10e1c5d708b16396ca45b8c590d3f
Doug Guthrie <doug.guthrie@fishtownanalytics.com>
2022-07-19 18:56:11 +0000

    test

--- models/staging/tpch/stg_tpch_customers.sql
+++ models/staging/tpch/stg_tpch_customers.sql
@@ -14,8 +14,7 @@ cleanup as (
         c_nationkey as nation_key,
         c_phone as phone_number,
         c_acctbal as account_balance,
-        c_mktsegment as market_segment,
-        c_comment as comment
+        c_mktsegment as market_segment
 
     from source
 
