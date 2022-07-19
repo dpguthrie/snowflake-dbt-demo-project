96ac85b2c6218f91aef6cf4d5bc64f5941c66873
Doug Guthrie <doug.guthrie@fishtownanalytics.com>
2022-07-19 22:11:30 +0000

    are you working

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -29,7 +29,6 @@ final as (
         {# region.region_key as region_key, #}
         region.name as region,
         customer.phone_number,
-        customer.account_balance,
         customer.market_segment
     from
         customer
