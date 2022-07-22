413daa0c02db741d0339358165b9449711d47abd
Doug Guthrie <douglas.p.guthrie@gmail.com>
2022-07-22 15:17:38 -0600

    now fix

--- models/staging/tpch/stg_tpch_orders.sql
+++ models/staging/tpch/stg_tpch_orders.sql
@@ -8,7 +8,7 @@ renamed as (
 
     select
     
-        o_orderkey as order_key
+        o_orderkey as order_key,
         o_custkey as customer_key,
         o_orderstatus as status_code,
         o_totalprice as total_price,
