10a865cd5569979ecf128eb8e89ac8f7a51f4a8d
Doug Guthrie <doug.guthrie@dbtlabs.com>
2022-07-27 17:51:49 +0000

    bad code

--- models/staging/tpch/stg_tpch_orders.sql
+++ models/staging/tpch/stg_tpch_orders.sql
@@ -8,8 +8,8 @@ renamed as (
 
     select
     
-        o_orderkey as order_key,
-        o_custkey as customer_key,
+        o_orderkey as order_key
+        , o_custkey as customer_key,
         o_orderstatus as status_code,
         o_totalprice as total_price,
         o_orderdate as order_date,
