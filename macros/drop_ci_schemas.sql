be6d51be1e07ab6aebecb22a89fa21e979c062fd
Doug Guthrie <douglas.p.guthrie@gmail.com>
2022-08-31 09:40:11 -0600

    Provide examples on how to use macro

--- macros/drop_ci_schemas.sql
+++ macros/drop_ci_schemas.sql
@@ -1,25 +1,38 @@
--- Drop schemas created during cloud CI runs
-{% macro drop_ci_schemas(databases, dryrun=True) %}
+{# drop_ci_schemas
+
+This macro drops all the schemas within a database beginning with "DBT_CLOUD_PR_". Use the dry_run param to see the schemas that will be dropped before dropping them.
+
+Args:
+    - database: string        -- the name of the database to clean. By default the target.database is used
+    - dry_run: bool           -- dry run flag. When dry_run is true, the cleanup commands are printed to stdout rather than executed. This is true by default
+
+Example 1 - dry run of current database
+    dbt run-operation drop_ci_schemas
+    
+Example 2 - actual run of current database
+    dbt run-operation drop_ci_schemas --args '{dry_run: False}'
+
+Example 3 - drop CI schemas from a different database
+    dbt run-operation drop_ci_schemas --args '{database: my_database, dry_run: False}'
+
+#}
+{% macro drop_ci_schemas(database=target.database, dryrun=True) %}
 
     {% set all_schemas_to_drop = [] %}
 
     {% set drop_schema_sql %}
 
-    {% for db in databases %}
+    {% set find_schemas_sql %}
+        SELECT SCHEMA_NAME
+        FROM {{ database | upper }}.INFORMATION_SCHEMA.SCHEMATA
+        WHERE SCHEMA_NAME LIKE 'DBT_CLOUD_PR_%';
+    {% endset %}
 
-        {% set find_schemas_sql %}
-            SELECT SCHEMA_NAME
-            FROM {{ db | upper }}.INFORMATION_SCHEMA.SCHEMATA
-            WHERE SCHEMA_NAME LIKE 'DBT_CLOUD_PR_%';
-        {% endset %}
+    {% set schemas_to_drop = run_query(find_schemas_sql) %}
 
-        {% set schemas_to_drop = run_query(find_schemas_sql) %}
-
-        {% for row in schemas_to_drop %}
-            DROP SCHEMA {{ db | upper }}.{{ row[0] | upper }};
-            {% do all_schemas_to_drop.append('{}.{}'.format(db.upper(), row[0].upper())) %}
-        {% endfor %}
-
+    {% for row in schemas_to_drop %}
+        DROP SCHEMA {{ database | upper }}.{{ row[0] | upper }};
+        {% do all_schemas_to_drop.append('{}.{}'.format(database.upper(), row[0].upper())) %}
     {% endfor %}
 
     {% endset %}
