8593d142f716dfb0efd377dcb7b4cae6a971e7a2
Doug Guthrie <douglas.p.guthrie@gmail.com>
2022-09-15 16:46:51 -0600

    Update stg_tpch_customers.sql

--- models/staging/tpch/stg_tpch_customers.sql
+++ models/staging/tpch/stg_tpch_customers.sql
@@ -15,8 +15,7 @@ cleanup as (
         c_phone as phone_number,
         c_acctbal as account_balance,
         c_mktsegment as market_segment,
-        c_comment as comment,
-        2 as my_favorite_number
+        c_comment as comment
 
     from source
 
