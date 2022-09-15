eb56720889e1f8d3ccbe9751a6c81d2c057b56ff
Doug Guthrie <douglas.p.guthrie@gmail.com>
2022-09-15 16:40:52 -0600

    Update stg_tpch_customers.sql

--- models/staging/tpch/stg_tpch_customers.sql
+++ models/staging/tpch/stg_tpch_customers.sql
@@ -15,8 +15,7 @@ cleanup as (
         c_phone as phone_number,
         c_acctbal as account_balance,
         c_mktsegment as market_segment,
-        c_comment as comment,
-        "new column" as new_column
+        c_comment as comment
 
     from source
 
