4533bfdc125342f1fbd7b4b4b20c237e73c00ebc
Doug Guthrie <douglas.p.guthrie@gmail.com>
2022-12-13 20:30:23 -0700

    Fix bugs in first iteration

--- scripts/multiple_ci.py
+++ scripts/multiple_ci.py
@@ -7,12 +7,17 @@ import time
 from dbtc import dbtCloudClient
 
 
+# Inputs
 CI_JOB_NAMING_CONVENTION = 'CI Job'
 CAUSE = 'Multiple CI Trigger'
+POLLING_INTERVAL = 10
+
+# Used in payload
 GITHUB_PULL_REQUEST_ID = os.getenv('GITHUB_PULL_REQUEST_ID', None)
 GIT_SHA = os.getenv('GIT_SHA', None)
 ACCOUNT_ID = os.getenv('DBT_CLOUD_ACCOUNT_ID', None)
-POLLING_INTERVAL = 10
+
+# Needed in list_runs endpoint
 COMPLETED_STATUSES = ['success', 'error', 'cancelled']
 
 
@@ -20,19 +25,19 @@ if __name__ == '__main__':
     
     # Initialize client
     client = dbtCloudClient()
-    account_id = os.environ['DBT_CLOUD_ACCOUNT_ID']
     
     # Get all the CI jobs across every project
-    jobs = client.cloud.list_jobs(account_id)
+    jobs = client.cloud.list_jobs(ACCOUNT_ID)
     total_job_count = jobs['extra']['pagination']['total_count']
     all_jobs = jobs['data']
     
     for offset in range(100, total_job_count, 100):
-        jobs = client.cloud.list_jobs(account_id, offset=offset)
+        jobs = client.cloud.list_jobs(ACCOUNT_ID, offset=offset)
         all_jobs.extend(jobs['data'])
         
     ci_jobs = [
-        job for job in all_jobs if CI_JOB_NAMING_CONVENTION.lower() in job['name']
+        job for job in all_jobs
+        if CI_JOB_NAMING_CONVENTION.lower() in job['name'].lower()
     ]
     
     # Create standard payload
@@ -49,8 +54,10 @@ if __name__ == '__main__':
     for ci_job in ci_jobs:
         schema_override = f'dbt_cloud_pr_{ci_job["id"]}_{GITHUB_PULL_REQUEST_ID}'
         payload.update({'schema_override': schema_override})
-        run = client.cloud.trigger_job(account_id, ci_job['id'], payload)
-        run_ids.append(run['id'])
+        run = client.cloud.trigger_job(
+            ACCOUNT_ID, ci_job['id'], payload, should_poll=False
+        )
+        run_ids.append(run['data']['id'])
         
     completed_dict = {s: [] for s in COMPLETED_STATUSES}
 
@@ -58,11 +65,11 @@ if __name__ == '__main__':
     while True:
         time.sleep(POLLING_INTERVAL)
         completed_runs = client.cloud.list_runs(
-            account_id, order_by='-id', status=COMPLETED_STATUSES
+            ACCOUNT_ID, order_by='-id', status=COMPLETED_STATUSES
         )['data']
         completed_runs_dict = {r['id']: r for r in completed_runs}
-        completed_runs_ids = completed_runs.keys()
-        for run_id in run_ids:
+        completed_runs_ids = completed_runs_dict.keys()
+        for run_id in run_ids[:]:
             if run_id in completed_runs_ids:
                 run = completed_runs_dict[run_id]
                 completed_dict[run['status_humanized'].lower()].append(run)
