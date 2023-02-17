3d94e8b22e3a401634a4c90b706316aa30f9011b
Doug Guthrie <douglas.p.guthrie@gmail.com>
2023-02-17 13:32:54 -0700

    Revert "Feat/test change tracking"

--- models/staging/tpch/stg_tpch_part_suppliers.sql
+++ models/staging/tpch/stg_tpch_part_suppliers.sql
@@ -8,7 +8,7 @@ renamed as (
 
     select
     
-        {{ dbt_utils.generate_surrogate_key(
+        {{ dbt_utils.surrogate_key(
             ['ps_partkey', 
             'ps_suppkey']) }} 
                 as part_supplier_key,
