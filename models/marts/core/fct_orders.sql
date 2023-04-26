942617daa9c3bdfaf46ee3fca209c24c730f64e4
Doug Guthrie <doug.guthrie@dbtlabs.com>
2023-04-26 14:42:16 +0000

    trivial change

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -44,7 +44,8 @@ final as (
         order_item_summary.gross_item_sales_amount,
         order_item_summary.item_discount_amount,
         order_item_summary.item_tax_amount,
-        order_item_summary.net_item_sales_amount
+        order_item_summary.net_item_sales_amount,
+        order_item_summary.gross_item_sales_amount * 2 as double_sales_amount
     from
         orders
         inner join order_item_summary
