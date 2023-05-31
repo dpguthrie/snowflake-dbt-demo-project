5afa129670f54bb47440e7e1c5bcdf9b9f9cadde
Doug Guthrie <doug.guthrie@dbtlabs.com>
2023-05-31 20:02:24 +0000

    um is there a change

--- models/staging/tpch/stg_tpch_customers.sql
+++ models/staging/tpch/stg_tpch_customers.sql
@@ -7,10 +7,10 @@ with source as (
 cleanup as (
 
     select
-    
+
         c_custkey as customer_key,
         c_name as name,
-        c_address as address, 
+        c_address as address,
         c_nationkey as nation_key,
         c_phone as phone_number,
         c_acctbal as account_balance,
