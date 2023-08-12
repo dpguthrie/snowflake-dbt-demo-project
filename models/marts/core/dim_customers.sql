8e626c5bf41dd281a1e52b10641bd6ec8b411001
Doug Guthrie <douglas.p.guthrie@gmail.com>
2023-08-12 06:17:06 -0600

    Update dim_customers.sql

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -24,9 +24,9 @@ final as (
         customer.customer_key,
         customer.name,
         customer.address,
-        {# nation.nation_key as nation_key, #}
+        nation.nation_key,
         nation.name as nation,
-        {# region.region_key as region_key, #}
+        region.region_key,
         region.name as region,
         customer.phone_number,
         customer.account_balance,
